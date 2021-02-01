a4a234c836e586dd5294b431662d7113c9dbc9dd
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-01 18:09:02 +0200

    Add GET endpoint for /invoices return empty JSON

--- src/main/java/com/neetfreek/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/neetfreek/MyFancyPdfInvoicesServlet.java
@@ -9,14 +9,22 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html; charset=UTF-8");
-        response.getWriter().print(
-                "<html>\n" +
-                        "<body>\n" +
-                        "<h1>Hello World</h1>\n" +
-                        "<p>This is my first embedded Tomcat HTML page!</p>\n" +
-                        "</body>\n" +
-                        "</html>"
-        );
+        // Write HTML greeting page to response for GET request for root endpoint
+        if (request.getRequestURI().equalsIgnoreCase("/")) {
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().print(
+                    "<html>\n" +
+                            "<body>\n" +
+                            "<h1>Hello World</h1>\n" +
+                            "<p>This is my first embedded Tomcat HTML page!</p>\n" +
+                            "</body>\n" +
+                            "</html>"
+            );
+        }
+        // Write empty json object to response for GET request for invoices endpoint
+        else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().print("[]");
+        }
     }
 }
