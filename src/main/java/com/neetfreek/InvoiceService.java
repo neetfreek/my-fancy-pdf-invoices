8d2165ff7d2d0851edc1a11e83683990afe4609f
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-02 19:13:08 +0200

    Fix TODO

--- src/main/java/com/neetfreek/InvoiceService.java
+++ src/main/java/com/neetfreek/InvoiceService.java
@@ -8,7 +8,7 @@ package com.neetfreek;
 public class InvoiceService {
 
     public Invoice create(String userId, Integer amount) {
-        //TODO: Implement create real PDF file, store on server, use link in constructor
+        //TODO: Implement create real PDF, store on server, use link in constructor
         return new Invoice(userId, "http://africau.edu/images/default/sample.pdf", amount);
     }
 }
