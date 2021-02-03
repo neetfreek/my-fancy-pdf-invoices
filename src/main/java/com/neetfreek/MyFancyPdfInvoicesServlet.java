168aa2305a00b811ee5466192a2ae1a78f407c1b
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-03 18:09:00 +0200

    Return JOSN formatted collection if invoices from GET /invoices endpoint

--- src/main/java/com/neetfreek/MyFancyPdfInvoicesServlet.java
+++ src/main/java/com/neetfreek/MyFancyPdfInvoicesServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class MyFancyPdfInvoicesServlet extends HttpServlet {
 
@@ -33,6 +34,10 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
                             "<p>This is my first embedded Tomcat HTML page!</p>\n" +
                             "</body>\n" +
                             "</html>");
+        } else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+            response.setContentType("application/json; charset=UTF-8");
+            List<Invoice> invoices = invoiceService.findAllInvoices();
+            response.getWriter().print(objectMapper.writeValueAsString(invoices));
         }
     }
 
@@ -46,9 +51,8 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
 
             Invoice invoice = invoiceService.create(userId, amount);
 
-            response.setContentType("application/jsonM charset=UTF-8");
-            String json = objectMapper.writeValueAsString(invoice);
-            response.getWriter().print(json);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().print(objectMapper.writeValueAsString(invoice));
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
