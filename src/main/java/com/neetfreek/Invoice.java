e208e9f62f1a50fc18b5eca88407a3dfe5a09d5e
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-03 17:52:18 +0200

    Format userId, pdfUrl JSON keys to user_id, pdf_url with Jackson annotations

--- src/main/java/com/neetfreek/Invoice.java
+++ src/main/java/com/neetfreek/Invoice.java
@@ -9,11 +9,19 @@
 
 package com.neetfreek;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class Invoice {
 
-    private String id, userId, pdfUrl;
+    private String id;
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("pdf_url")
+    private String pdfUrl;
+
     private Integer amount;
 
     public Invoice() {
