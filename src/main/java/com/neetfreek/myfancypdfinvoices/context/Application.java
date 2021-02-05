3aab3ebf6509b378819180052b3b95355a227726
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-05 16:12:01 +0200

    Add UserService to Application for dependency management

--- src/main/java/com/neetfreek/myfancypdfinvoices/context/Application.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/context/Application.java
@@ -11,9 +11,12 @@ package com.neetfreek.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neetfreek.myfancypdfinvoices.service.InvoiceService;
+import com.neetfreek.myfancypdfinvoices.service.UserService;
 
 public class Application {
 
     public static final InvoiceService invoiceService = new InvoiceService();
+    public static final UserService userService = new UserService();
+
     public static final ObjectMapper objectMapper = new ObjectMapper();
 }
