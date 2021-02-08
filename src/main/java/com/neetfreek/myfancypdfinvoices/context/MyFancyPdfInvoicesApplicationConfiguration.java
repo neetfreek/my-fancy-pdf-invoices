b5f2f1ddb8a02ad287cac54a993e48f4f1114bed
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-08 18:17:33 +0200

    Explicitly define @Beans' scopes as singleton

--- src/main/java/com/neetfreek/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -5,6 +5,7 @@
 
 /*
  * Explicit JavaConfig for Spring's ApplicationContext
+ *      @Scope for @Beans specified as singleton scope (SCOPE_SINGLETON) for clarity
  */
 
 package com.neetfreek.myfancypdfinvoices.context;
@@ -13,23 +14,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neetfreek.myfancypdfinvoices.service.InvoiceService;
 import com.neetfreek.myfancypdfinvoices.service.UserService;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public UserService userService() {
         return new UserService();
     }
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public InvoiceService invoiceService(UserService userService) {
         return new InvoiceService(userService);
     }
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
