8e3e5ccac85d36a28ae6b5cbfc4f8de8e0c85080
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-20 00:01:07 +0200

    Create HTTP GET @Controller for invoices

--- src/main/java/com/neetfreek/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -5,11 +5,13 @@
 
 /*
  * Explicit JavaConfig for Spring's ApplicationContext
- *      @ComponentScan finds @Component, @Component specialisation classes in basePackage
- *          - UserService
- *          - InvoiceService
- *      @PropertySource overwritten to application-dev.properties if profile changed with e.g.
- *          -Dspring.profiles.active=dev
+ * @ComponentScan finds @Component, @Component specialisation classes in basePackage
+ *      - UserService
+ *      - InvoiceService
+ * @PropertySource overwritten to application-dev.properties if profile changed with e.g.
+ *      -Dspring.profiles.active=dev
+ * @EnableWebMvc ensures Spring WebMVC initialized with sane default configuration (including, for instance, JSON
+ *      conversion from e.g. Invoices' CopyOnWriteArrayList<> to JSON provided jackson dependency in classpath
  */
 
 package com.neetfreek.myfancypdfinvoices.context;
@@ -19,11 +21,13 @@ import com.neetfreek.myfancypdfinvoices.ApplicationLauncher;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
+@EnableWebMvc
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
---
+++ src/main/java/com/neetfreek/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,49 @@
+/*
+ * Jonathan Widdowson
+ * Neetfreek, 2021
+ */
+
+/*
+ * @RestController (i.e. @Controller and @ResponseBody) on class applies to all methods, designating them as returning
+ *      JSON, XML, and plain text in response body to client
+ *      - Internally handles request URI parsing JSON conversion
+ *      - The implicit inclusion of @ResponseBody means no HTML templating framework is used for responses
+ * @GetMapping("/[endpoint]") shorthand for @RequestMapping(value = "/[endpoint]", method = RequestMethod.GET)
+ */
+
+package com.neetfreek.myfancypdfinvoices.web;
+
+import com.neetfreek.myfancypdfinvoices.model.Invoice;
+import com.neetfreek.myfancypdfinvoices.service.InvoiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyFancyPdfInvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "<html lang=\"en-GB\">\n" +
+                "<body>\n" +
+                "<h1>Hello World v2</h1>\n" +
+                "<p>This is my first embedded Tomcat HTML page!</p>\n" +
+                "<p>Now improved with @controllers instead of httpServlets :)</p>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+
+    @GetMapping("/invoices")
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+}
