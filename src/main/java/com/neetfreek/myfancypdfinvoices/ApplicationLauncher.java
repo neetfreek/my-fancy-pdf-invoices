28f36801744ebe0ddf0cc657b4e317c80274d805
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-19 19:38:07 +0200

    Create @Controller for HTTP GET request to /

--- src/main/java/com/neetfreek/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/ApplicationLauncher.java
@@ -5,15 +5,26 @@
 
 /*
  * Launch application by running main()
+ *      dispatcherServlet serves as Spring WebMVC entry point, acting as servlet accepting all HTTP requests to,
+ *          and sending responses from, @Controllers
+ *      dispatcherServlet knows about application context as passed via MyFancyPdfInvoicesApplicationConfiguration.class
+ *      webAppCtx knows about @Controllers and @...Mappings because of @ComponentScanning
  */
 
 package com.neetfreek.myfancypdfinvoices;
 
+import com.neetfreek.myfancypdfinvoices.context.MyFancyPdfInvoicesApplicationConfiguration;
 import com.neetfreek.myfancypdfinvoices.web.MyFancyPdfInvoicesServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
 
 public class ApplicationLauncher {
 
@@ -22,11 +33,24 @@ public class ApplicationLauncher {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context ctx = tomcat.addContext("", null);
-        Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+        Context tomcatCtx = tomcat.addContext("", null);
+
+        WebApplicationContext webAppCtx = createWebApplicationContext(tomcatCtx.getServletContext());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppCtx);
+        // Wrapper servlet = Tomcat.addServlet(tomcatCtx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+        Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");
 
         tomcat.start();
+    }
+
+    public static WebApplicationContext createWebApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyFancyPdfInvoicesApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
     }
 }
