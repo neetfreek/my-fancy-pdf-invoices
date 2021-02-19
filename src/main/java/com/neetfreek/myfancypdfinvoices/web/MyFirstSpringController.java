f8d9d3497991a45f41b072ee95ee1128bedb0af7
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-02-19 23:28:21 +0200

    Update response body for HTTP GET / request

--- src/main/java/com/neetfreek/myfancypdfinvoices/web/MyFirstSpringController.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/web/MyFirstSpringController.java
@@ -7,6 +7,7 @@ package com.neetfreek.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,6 +16,12 @@ public class MyFirstSpringController {
     @GetMapping("/")
     @ResponseBody
     public String index(){
-        return "Hello world";
+        return "<html lang=\"en-GB\">\n" +
+                "<body>\n" +
+                "<h1>Hello World v2</h1>\n" +
+                "<p>This is my first embedded Tomcat HTML page!</p>\n" +
+                "<p>Now improved with @controllers instead of httpServlets :)</p>\n" +
+                "</body>\n" +
+                "</html>";
     }
 }
