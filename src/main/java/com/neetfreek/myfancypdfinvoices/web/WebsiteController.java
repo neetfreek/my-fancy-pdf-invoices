4b7af83a982817011ec79fd78f4c927580e017ec
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-03-07 12:14:54 +0200

    Implement simple templating for dynamic homepage content

--- src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
@@ -11,14 +11,22 @@
 
 package com.neetfreek.myfancypdfinvoices.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
 
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model,
+                           @RequestParam(required = false, defaultValue = ("${default.username}")) String username) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", new Date());
         return "index.html";
     }
 }
