eb9a3f5da24daf274d0ebccd4c0ab16f87c833e3
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-03-07 18:08:08 +0200

    Add login page with template, LoginForm model

--- src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
@@ -11,9 +11,12 @@
 
 package com.neetfreek.myfancypdfinvoices.web;
 
+import com.neetfreek.myfancypdfinvoices.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
@@ -27,5 +30,21 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", new Date());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+        // For development assume username == password is correct (instead of DB call)
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", true);
+        return "login.html";
     }
 }
