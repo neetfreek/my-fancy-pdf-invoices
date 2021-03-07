7968c3459e7a8fc81d76c3ae2ba9ebe5f7914238
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2021-03-07 12:15:12 +0200

    Remove unused import

--- src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/neetfreek/myfancypdfinvoices/web/WebsiteController.java
@@ -11,7 +11,6 @@
 
 package com.neetfreek.myfancypdfinvoices.web;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
